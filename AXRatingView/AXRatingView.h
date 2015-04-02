fc9bb3dcf2c75bc3ce2ab67e432403754728dd17
Felipe Baytelman <felipe.baytelman@gmail.com>
2015-04-01 17:58:01 -0700

    Remove markfont name

--- AXRatingView/AXRatingView.h
+++ AXRatingView/AXRatingView.h
@@ -12,8 +12,6 @@
 @property (nonatomic) NSUInteger numberOfStar;
 @property (copy, nonatomic) NSString *markCharacter;
 @property (strong, nonatomic) UIFont *markFont;
-@property (nonatomic, assign) CGFloat markFontSize;
-@property (nonatomic, assign) NSString * markFontName;
 @property (strong, nonatomic) UIImage *markImage;
 @property (strong, nonatomic) UIColor *baseColor;
 @property (strong, nonatomic) UIColor *highlightColor;
--- AXRatingView/AXRatingView.m
+++ AXRatingView/AXRatingView.m
@@ -159,19 +159,6 @@
   }
 }
 
-- (void)setMarkFontName:(NSString *)markFontName
-{
-  UIFont * font = [UIFont fontWithName:markFontName size:_markFont.pointSize];
-  if (font) {
-    self.markFont = font;
-  }
-}
-
-- (void)setMarkFontSize:(CGFloat)markFontSize
-{
-  self.markFont = [self.markFont fontWithSize:markFontSize];
-}
-
 - (void)setMarkFont:(UIFont *)markFont
 {
   if (_markFont != markFont) {
